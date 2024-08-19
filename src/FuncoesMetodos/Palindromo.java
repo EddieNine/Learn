e22275279146886f4d8d9f4381384e5c151c1682
EddieNine <believefixed@hotmail.com>
2024-08-19 15:32:16 -0300

    Desafio 2: Verificador de Palíndromo

---
+++ src/FuncoesMetodos/Palindromo.java
@@ -0,0 +1,19 @@
+package FuncoesMetodos;
+
+public class Palindromo {
+    public boolean isPalindromo(String palavra) {
+        String invertida = new StringBuilder(palavra).reverse().toString();
+        return palavra.equalsIgnoreCase(invertida);
+    }
+
+    public static void main(String[] args) {
+        Palindromo verificador = new Palindromo();
+
+        String palavra = "Radar";
+        if(verificador.isPalindromo(palavra)) {
+            System.out.println(palavra + " é um palíndromo.");
+        } else {
+            System.out.println(palavra + " não é um palíndromo.");
+        }
+    }
+}
