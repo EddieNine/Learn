78b145204fcdd1e3257c86f39511120c0440ffaf
EddieNine <believefixed@hotmail.com>
2024-08-21 02:51:07 -0300

    Desafio 1: Manipulação de Arrays

--- src/ArraysColecoes/ArrayDesafios.java
+++ src/ArraysColecoes/ArrayDesafios.java
@@ -2,25 +2,25 @@ package ArraysColecoes;
 
 public class ArrayDesafios {
     public static void main(String[] args) {
-        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+       int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         System.out.println("Soma: " + somaArrays(numeros));
-        System.out.println("Maior valor: " + maiorValorArray(numeros));
+        System.out.println("Maior valor: " + maiorValorArrays(numeros));
     }
 
     public static int somaArrays(int[] arrays) {
         int soma = 0;
-        for (int numeros : arrays) {
-            soma += numeros;
+        for (int numero : arrays) {
+            soma += numero;
         }
         return soma;
     }
 
-    public static int maiorValorArray(int[] arrays) {
-        int maior = arrays[0];
-        for (int numeros : arrays) {
-            if (numeros > maior) {
-                maior = numeros;
+    public static int maiorValorArrays(int[] arrays) {
+        int maior = 0;
+        for (int numero : arrays) {
+            if (numero > maior) {
+                maior = numero;
             }
         }
         return maior;
