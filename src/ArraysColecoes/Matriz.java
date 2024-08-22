75fe2dff2cc21d36e1f1dbea7ad2c69cb5c709f8
EddieNine <believefixed@hotmail.com>
2024-08-22 02:44:35 -0300

    Desafio 3: Matriz (Array Multidimensional)

--- src/ArraysColecoes/Matriz.java
+++ src/ArraysColecoes/Matriz.java
@@ -8,8 +8,8 @@ public class Matriz {
     }
 
     public static void imprimirMatriz(int[][] matriz) {
-        for(int i = 0; i < matriz.length; i++) {
-            for(int j = 0; j < matriz[i].length; j++){
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
                 System.out.print(matriz[i][j] + "\t");
             }
             System.out.println();
