cd01a7b7241088fa7bf0eb4f0545b53e5730dfa0
EddieNine <believefixed@hotmail.com>
2024-08-31 09:31:52 -0300

    Caixa de Diálogo Simples de Mensagem

---
+++ src/GUI/CaixaDialogo/DialogoMensagem.java
@@ -0,0 +1,9 @@
+package GUI.CaixaDialogo;
+
+import javax.swing.*;
+
+public class DialogoMensagem {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Bem-vindo ao Java Swing!");
+    }
+}
