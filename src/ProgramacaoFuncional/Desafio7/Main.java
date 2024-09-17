cdfbc8f345bbef87177cb8f3e7e9432db6255022
EddieNine <believefixed@hotmail.com>
2024-09-17 15:32:11 -0300

    Remover Duplicatas

---
+++ src/ProgramacaoFuncional/Desafio7/Main.java
@@ -0,0 +1,16 @@
+package ProgramacaoFuncional.Desafio7;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 1, 1, 2, 5, 6, 5, 6, 8, 9, 10, 5, 6, 3, 8, 8);
+        
+        List<Integer> removerDuplicados = numeros.stream()
+                .distinct()
+                .sorted()
+                .toList();
+        System.out.println("Valores duplicados removidos: " + removerDuplicados);
+    }
+}
