4dfd17a41df99742602130c1403ed2094e61dffe
EddieNine <believefixed@hotmail.com>
2024-09-19 14:04:02 -0300

    Tabelas Hash (Hash Tables)

---
+++ src/EstruturaDeDados/HashTables/TabelaHashExemplo.java
@@ -0,0 +1,15 @@
+package EstruturaDeDados.HashTables;
+
+import java.util.HashMap;
+
+public class TabelaHashExemplo {
+    public static void main(String[] args) {
+        HashMap<String, Integer> tabela = new HashMap<>();
+        
+        tabela.put("João", 30);
+        tabela.put("Maria", 25);
+        tabela.put("Pedro", 35);
+
+        System.out.println("Idade de João: " + tabela.get("João"));
+    }
+}
