122e3ed070fa8d79bffc2faa5b3a8f208b034306
EddieNine <believefixed@hotmail.com>
2024-09-28 13:59:51 -0300

    Criar uma Janela Simples com Botão

---
+++ src/DesafiosExtras/JanelaSimplesComBotao/JanelaSimples.java
@@ -0,0 +1,30 @@
+package DesafiosExtras.JanelaSimplesComBotao;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class JanelaSimples extends JFrame {
+    
+    public JanelaSimples() {
+        setTitle("Minha janela simples");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 200);
+        
+        JButton button = new JButton("Clique Aqui");
+        
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Botão clickado!");
+            }
+        });
+        
+        add(button);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new JanelaSimples();
+    }
+}
