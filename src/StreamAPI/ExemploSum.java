b68447e9c626b49d8c9352ab30797bb1a3b6bb76
EddieNine <believefixed@hotmail.com>
2024-10-21 16:05:29 -0300

    Streams

---
+++ src/StreamAPI/ExemploSum.java
@@ -0,0 +1,17 @@
+package StreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ExemploSum {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        int sum = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        System.out.println("Sum of even numbers: " + sum);
+    }
+}
