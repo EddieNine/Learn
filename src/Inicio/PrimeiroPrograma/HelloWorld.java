e35d3de37f133ef6db07872494648e3cc0db4b8d
EddieNine <believefixed@hotmail.com>
2025-04-01 17:03:36 -0300

    Primeiro Programa em Java

---
+++ src/Inicio/PrimeiroPrograma/HelloWorld.java
@@ -0,0 +1,8 @@
+package Inicio.PrimeiroPrograma;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Meu nome é Edcarlos e tenho 25 anos.");
+        System.out.println("Meu objetivo ao aprender Java é me tornar um desenvolvedor altamente qualificado.");;
+    }
+}
