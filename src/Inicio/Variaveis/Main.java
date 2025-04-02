7259f7c5d26d86a0a8bc24ac4d1d771ccbfe84e2
EddieNine <believefixed@hotmail.com>
2025-04-02 15:45:17 -0300

    Variáveis e Tipos de Dados | Desafio 1

---
+++ src/Inicio/Variaveis/Main.java
@@ -0,0 +1,15 @@
+package Inicio.Variaveis;
+
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Edcarlos";
+        double peso = 95;
+        double altura = 1.85;
+        boolean estudandoJava = true;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Peso: " + peso + " kg");
+        System.out.println("Altura: " + altura + " m");
+        System.out.println("Está estudando Java? " + estudandoJava);
+    }
+}
