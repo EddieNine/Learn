f4c38626c1383fba1b2b061dfd4425da3979a965
EddieNine <believefixed@hotmail.com>
2025-04-02 16:58:00 -0300

    Operadores | Desafio 3

---
+++ src/Inicio/Operadores/Main3.java
@@ -0,0 +1,12 @@
+package Inicio.Operadores;
+
+public class Main3 {
+    public static void main(String[] args) {
+        int x = 10, y = 15;
+
+        System.out.println("x == y: " + (x == y));
+        System.out.println("x != y: " + (x != y));
+        System.out.println("x > y: " + (x > y));
+        System.out.println("x < y: " + (x < y));
+    }
+}
