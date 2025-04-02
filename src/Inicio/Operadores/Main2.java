feff754c3960e92916ce0d6856253abe88ae400f
EddieNine <believefixed@hotmail.com>
2025-04-02 16:55:07 -0300

    Operadores | Desafio 2

---
+++ src/Inicio/Operadores/Main2.java
@@ -0,0 +1,19 @@
+package Inicio.Operadores;
+
+public class Main2 {
+    public static void main(String[] args) {
+        double x = 15;
+        double y = 10;
+        double soma = x + y;
+        double subtracao = x - y;
+        double multiplicacao = x * y;
+        double divisao = x / y;
+        double modulo = x % y;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+        System.out.println("Módulo (resto da divisão): " + modulo);
+    }
+}
