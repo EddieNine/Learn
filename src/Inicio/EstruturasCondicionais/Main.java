1764aadd53822fe7670858a0672745bf3c3aaf4f
EddieNine <believefixed@hotmail.com>
2025-04-03 14:09:46 -0300

    Estruturas Condicionais (if/else e switch) | Desafio 1

---
+++ src/Inicio/EstruturasCondicionais/Main.java
@@ -0,0 +1,14 @@
+package Inicio.EstruturasCondicionais;
+
+public class Main {
+    public static void main(String[] args) {
+        int numero = 7;
+        if(numero > 0) {
+            System.out.println("Número posítivo.");
+        } else if (numero < 0) {
+            System.out.println("Número é negativo.");
+        } else {
+            System.out.println("Número é zero.");
+        }
+    }
+}
