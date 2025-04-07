8d2b3455e9c53ef6daea2206ce99933841741245
EddieNine <believefixed@hotmail.com>
2025-04-07 15:10:17 -0300

    Arrays (Vetores) | Desafio 1

---
+++ src/Inicio/Arrays/Main.java
@@ -0,0 +1,11 @@
+package Inicio.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] nomes = {"Edcarlos", "Daniel", "Steve", "João","David"};
+
+        for (String nome : nomes) {
+            System.out.println("Nome: " + nome);
+        }
+    }
+}
