2037e4250c4cd51a6c056615072a3c48420c1ffe
EddieNine <believefixed@hotmail.com>
2025-04-07 15:06:19 -0300

    Laços de Repetição (Loops: for, while, do-while) | Desafio 2

---
+++ src/Inicio/LacosRepeticao/Main2.java
@@ -0,0 +1,13 @@
+package Inicio.LacosRepeticao;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int contador = 2;
+        while (contador <= 20) {
+            if (contador % 2 == 0) {
+                System.out.println(contador);
+            }
+            contador++;
+        }
+    }
+}
