224334a2a1c3e785f57a2290474e8e62e859691e
EddieNine <believefixed@hotmail.com>
2025-04-08 15:26:36 -0300

    Orientação a Objetos - Parte 1: Classes e Objetos | Desafio 1

---
+++ src/Inicio/ClassesObjetos/Carro.java
@@ -0,0 +1,10 @@
+package Inicio.ClassesObjetos;
+
+public class Carro {
+    String marca;
+    int ano;
+
+    void informacoes() {
+        System.out.println("Este carro é da marca " + marca + " e foi fabricado no ano " + ano + ".");
+    }
+}
---
+++ src/Inicio/ClassesObjetos/Main.java
@@ -0,0 +1,11 @@
+package Inicio.ClassesObjetos;
+
+public class Main {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.marca = "NoName";
+        carro.ano = 1999;
+
+        carro.informacoes();
+    }
+}
