373903b973a1ee4fd40f892ce55777906d358ef5
EddieNine <believefixed@hotmail.com>
2025-04-08 15:13:06 -0300

    Métodos (Funções) | Desafio 2

---
+++ src/Inicio/Metodos/Main2.java
@@ -0,0 +1,12 @@
+package Inicio.Metodos;
+
+public class Main2 {
+    public static int dobro(int numero){
+        return numero * 2;
+    }
+    
+    public static void main(String[] args) {
+        int resultado = dobro(5);
+        System.out.println("O dobro vale " + resultado);
+    }
+}
