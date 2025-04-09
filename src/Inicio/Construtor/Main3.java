b4b56fe373ee0619962f4f507f861eb50e1f4925
EddieNine <believefixed@hotmail.com>
2025-04-09 17:07:46 -0300

    Construtores e Encapsulamento | Desafio 3

---
+++ src/Inicio/Construtor/Main3.java
@@ -0,0 +1,10 @@
+package Inicio.Construtor;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Produto produto = new Produto("Caderno", -5.0);
+
+        System.out.println("Produto: " + produto.getNome());
+        System.out.println("Preço: R$" + produto.getPreco());
+    }
+}
---
+++ src/Inicio/Construtor/Produto.java
@@ -0,0 +1,33 @@
+package Inicio.Construtor;
+
+public class Produto {
+    private String nome;
+    private double preco;
+
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        setPreco(preco);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setPreco(double preco) {
+        if (preco < 0) {
+            System.out.println("Não é permitido valor negativo.");
+            this.preco = 0.0;
+        } else {
+            this.preco = preco;
+        }
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+}
