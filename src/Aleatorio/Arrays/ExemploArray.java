da75ad1ce0e91d324922058c6dc6b7b88787053c
EddieNine <believefixed@hotmail.com>
2025-04-21 16:54:09 -0300

    Exemplo de Array

---
+++ src/Aleatorio/Arrays/ExemploArray.java
@@ -0,0 +1,11 @@
+package Aleatorio.Arrays;
+
+public class ExemploArray {
+    public static void main(String[] args) {
+        String[] nomes = {"Ana", "João", "Maria", "Carlos"};
+
+        for(int i = 0; i < nomes.length; i++) {
+            System.out.println("Nome " + i + ": " + nomes[i]);
+        }
+    }
+}
