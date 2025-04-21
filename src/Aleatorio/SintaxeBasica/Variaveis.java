20a393e73f46b1992d5cfb8e9ba45a7432c3c707
EddieNine <believefixed@hotmail.com>
2025-04-21 18:08:28 -0300

    Sintaxe Básica em Java

---
+++ src/Aleatorio/SintaxeBasica/Variaveis.java
@@ -0,0 +1,12 @@
+package Aleatorio.SintaxeBasica;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        int idade = 25; // tipo inteiro
+        float salario = 3500.50f; // tipo float (usa o "f" para indicar que é float)
+        double preco  = 9.99; // tipo double (números de ponto flutuante mais precisos)
+        boolean isAtivo = true; // tipo booleano (verdadeiro ou falso)
+        String nome = "João"; // tipo String (sequência de caracteres)
+
+    }
+}
