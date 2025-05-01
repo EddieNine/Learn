68825db8ea110ba19b1f31ecb608c327e543df72
EddieNine <believefixed@hotmail.com>
2025-05-01 09:01:20 -0300

    Introdução a POO Construtores e Encapsulamento.

---
+++ src/cursoBackend/Semana2/IntroducaoPOO/Main.java
@@ -0,0 +1,9 @@
+package cursoBackend.Semana2.IntroducaoPOO;
+
+public class Main {
+    public static void main(String[] args) {
+        Carro meuCarro = new Carro("Toyota", "Corolla", 2020);
+        meuCarro.ligar();
+        meuCarro.exibirInformacoes();
+    }
+}
